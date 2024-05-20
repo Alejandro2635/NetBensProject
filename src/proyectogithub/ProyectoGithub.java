eb1767e588e7b9577d3cf743585fb20e95b30216
perei <perei@desktop>
2024-05-20 11:39:12 +0200

    Segundo commit

--- src/proyectogithub/ProyectoGithub.java
+++ src/proyectogithub/ProyectoGithub.java
@@ -14,7 +14,7 @@ public class ProyectoGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ProyectoGithub program = new ProyectoGithub();
     }
     
 }
